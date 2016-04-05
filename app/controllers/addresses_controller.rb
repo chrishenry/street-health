00e8249340709430591aa815e412caadf747f11b
Chris Henry <henry.christopher@gmail.com>
2016-04-04 23:58:22 -0400

    new: Basic lookup of address by address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,11 @@
 class AddressesController < ApplicationController
+
   def index
+    @addresses = if params[:address]
+                 Address.where('address LIKE ?',"%#{params[:address]}%")
+               else
+                 []
+               end
   end
+
 end
