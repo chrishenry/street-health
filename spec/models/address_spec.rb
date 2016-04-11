e9fadb8caa8b6bb67e16c109bf7931d96a5f1758
Chris Henry <henry.christopher@gmail.com>
2016-04-11 16:06:53 -0400

    new: Confirm validation function does nothing if it passes

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -14,7 +14,7 @@ end
 
 RSpec.describe Address, type: :model do
 
-  it "properly validates geocoder data" do
+  it "raises exception if fields aren't present" do
 
     best_match = BestMatchMock.new({"address_components"=>
       [{"long_name"=>"New York",
@@ -25,25 +25,22 @@ RSpec.describe Address, type: :model do
         "types"=>["administrative_area_level_1", "political"]},
        {"long_name"=>"United States",
         "short_name"=>"US",
-        "types"=>["country", "political"]}],
-     "formatted_address"=>"New York, NY, USA",
-     "geometry"=>
-      {"bounds"=>
-        {"northeast"=>{"lat"=>40.91525559999999, "lng"=>-73.70027209999999},
-         "southwest"=>{"lat"=>40.4960439, "lng"=>-74.2557349}},
-       "location"=>{"lat"=>40.7127837, "lng"=>-74.0059413},
-       "location_type"=>"APPROXIMATE",
-       "viewport"=>
-        {"northeast"=>{"lat"=>40.91525559999999, "lng"=>-73.70027209999999},
-         "southwest"=>{"lat"=>40.4960439, "lng"=>-74.2557349}}},
-     "partial_match"=>true,
-     "place_id"=>"ChIJOwg_06VPwokRYv534QaPC8g",
-     "types"=>["locality", "political"]})
+        "types"=>["country", "political"]}]
+        })
 
     expect{
       Address.validate_address_components(best_match)
     }.to raise_exception(ActiveRecord::RecordNotFound)
 
+  end
+
+  it "doesn't raise exception if all fields required are present" do
+    best_match = BestMatchMock.new({"address_components"=>
+      [{"long_name"=>"New York",
+        "short_name"=>"New York",
+        "types"=>["route", "street_number", "neighborhood", "postal_code"]}]})
+
+    Address.validate_address_components(best_match)
   end
 
 end
